6040f36ca689702e48720c648fb14326371e86a5
Aleksa0308 <aleksas0308@gmail.com>
2024-11-08 01:17:09 +0100

    feat: admin-panel: - dashboard data for performance - monthly order number line chart data - monthly bruto earnings line chart data

--- backend/src/modules/dashboard/dashboard.controller.ts
+++ backend/src/modules/dashboard/dashboard.controller.ts
@@ -1,16 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { DashboardService } from './dashboard.service';
+import { SkipAuth } from 'src/core/guards/auth-guard';
+import { CurrentUser } from 'src/commons';
+import { CurrentUserDto } from '../auth/dto/current-user.dto';
 
-@ApiTags('users')
+@ApiTags('dashboard')
 @ApiBearerAuth()
 @Controller('dashboard')
 export class DashboardController {
     constructor(private readonly dashboardService: DashboardService){}
 
-    @Get()
-    async getDashboardData(){
-        return this.dashboardService.getDashboardData();
+    @Get("performance")
+    async getPerformanceData(@CurrentUser() currentUser: CurrentUserDto){
+        return this.dashboardService.getPerformanceData(currentUser);
     }
 
+    @Get("monthly-stats/orders")
+    async getMonthlyStats(@CurrentUser() currentUser: CurrentUserDto){
+        return this.dashboardService.getMonthlyOrderNumberStats(currentUser);
+    }
+
+    @Get("monthly-stats/earnings")
+    async getMonthlyEarnings(@CurrentUser() currentUser: CurrentUserDto){
+        return this.dashboardService.getMonthlyEarningsStats(currentUser);
+    }
 }
--- backend/src/modules/dashboard/dashboard.module.ts
+++ backend/src/modules/dashboard/dashboard.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
 
+
 @Module({
-  providers: [DashboardService],
+  providers: [
+    DashboardService 
+  ],
   controllers: [DashboardController]
 })
 export class DashboardModule {}
