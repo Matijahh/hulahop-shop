dec22f5456e35521f6f0a3a61e1005c1fafb8ee8
Aleksa0308 <aleksas0308@gmail.com>
2024-11-09 19:59:10 +0100

    feat: - modified the dashboard calls to use quanitity - modified the response to work with recharts - modified forntend for loading chart data

---
+++ frontend/src/Views/Admin/Colors/styled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const TABLE_OFFSET = "154px";
+
+export const ColorsContainer = styled.div`
+  .colors-table {
+    height: calc(100vh - ${TABLE_OFFSET});
+
+    .css-yrdy0g-MuiDataGrid-columnHeaderRow {
+      .MuiDataGrid-withBorderColor:nth-child(2),
+      .MuiDataGrid-withBorderColor:nth-child(5),
+      .MuiDataGrid-withBorderColor:last-child {
+        .MuiDataGrid-columnHeaderTitleContainer {
+          justify-content: center;
+        }
+      }
+    }
+  }
+`;
---
+++ frontend/src/Views/Admin/ReturnedProducts/index.jsx
@@ -0,0 +1,50 @@
+import { useTranslation } from "react-i18next";
+import { renderHeader } from "./mock";
+import muska4 from "../../../assets/images/muska-4.jpg";
+import map from "lodash/map";
+
+import { CommonWhiteBackground, FlexBox } from "../../../components/Sections";
+
+import InputComponent from "../../../components/InputComponent";
+import Tables from "../../../components/SuperAdmin/Tables";
+
+const ReturnedProducts = () => {
+  const { t } = useTranslation();
+
+  const renderData = map(Array(10), (item, index) => ({
+    ...item,
+    no: `${index + 1}`,
+    user_name: "Demo User",
+    product_name: "Blue T-Shirt",
+    product_image: muska4,
+    product_description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    reason: "Dolor sit amet consectetur adipisicing elit.",
+    id: 101 * index,
+    price: 1200,
+    category: "T-shirt",
+    order_date: "02/10/2023",
+    returned_date: "07/10/2023",
+    qty: "2",
+    status: "Returned",
+  }));
+
+  return (
+    <CommonWhiteBackground>
+      <FlexBox className="mb-4">
+        <div className="main-title ">{t("Returned Products")}</div>
+
+        <InputComponent type="search" label={t("Search Orders")} />
+      </FlexBox>
+      <Tables
+        body={renderData}
+        header={renderHeader.map((item) => ({
+          ...item,
+          headerName: t(item.headerName),
+        }))}
+      />
+    </CommonWhiteBackground>
+  );
+};
+
+export default ReturnedProducts;
